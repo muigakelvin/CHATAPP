9748b0ce5a1c55595a2c06c5852654076c6b44e6
muigakelvin <kelvinkimondomuiga@gmail.com>
2023-10-19 21:27:19 +0300

    side bar edits

---
+++ index.js
@@ -0,0 +1,24 @@
+//SIDEBAR
+const menuItems = document.querySelectorAll('.menu-item');
+
+
+//remove active class from all menu item
+const changeActiveItem = () =>{
+    menuItems.forEach(item=>{
+        item.classList.remove('active')
+    })
+}
+
+menuItems.forEach(item =>{
+    item.addEventListener('click',() => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications'){
+            document.querySelector('.notifications-popup').style.display = 'none';
+        }else{
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+            //document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
